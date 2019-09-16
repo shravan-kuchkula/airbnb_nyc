3fa9b1421c5a9c8aacf370d6165471b3ddb0f3df
Shravan Kuchkula <Shravan.Kuchkula@gmail.com>
2019-09-16 14:02:21 -0400

    Fleshing out the project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of New York City's Airbnb data',
+    author='Shravan Kuchkula',
+    license='',
+)
